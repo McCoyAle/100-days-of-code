2dcc7e1db77ff0f129652c5dc7f7fb8ce643ed15
Alexandra McCoy <mccoyale@gmail.com>
2019-03-20 13:33:50 -0400

    Adding the folder for Python files and 2 exercises

---
+++ python-hardway/ex2.py
@@ -0,0 +1,8 @@
+# Example focuses on commenting
+
+print "I could have code like this." # A comment after is also ignored.
+
+# You can use comments to comment out a piece of code. (Disable the code)
+# print "This won't run"
+
+print "This will run."
