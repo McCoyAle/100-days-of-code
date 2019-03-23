b2832a93db0a30ce84e8f751c5a9dba222cc42c1
Alexandra McCoy <mccoyale@gmail.com>
2019-03-22 23:35:05 -0400

    Adding exercise 5 and directory.

---
+++ python-hardway/ex5/ex5.py
@@ -0,0 +1,18 @@
+
+my_name = 'Alexandra McCoy'
+my_age = 31
+my_height = 66 # inches
+my_weight = 161 # lbs
+my_eyes = 'Brown'
+my_teeth = 'White'
+my_hair = 'Black'
+
+print "Let's talk about %s." % my_name
+print "She's %d inches tall." % my_height
+print "She's %d pounds heavy." % my_weight
+print "Actually, that's not too heavy."
+print "He's got %s eyes and %s hair." % (my_eyes, my_hair)
+print "His teeth are usually %s depending on the coffee." % my_teeth
+
+# This line is tricky. Try to get it correct.
+print "If I add %d, %d, and %d I get %d." % (my_age, my_height, my_weight, my_age + my_height + my_weight)
