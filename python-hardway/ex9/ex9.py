dc48af2bceab9af62e93d26da6e541f2d17e286e
Alexandra McCoy <mccoyale@gmail.com>
2019-03-28 14:06:44 -0400

    Exercise 9 and it's example

---
+++ python-hardway/ex9/ex9.py
@@ -0,0 +1,17 @@
+# Creates a variable called days.
+days = "Mon, Tues, Wed, Thur, Fri, Sat, Sun"
+#Creates a variable called months and then lists the months provided, but each on a new line.
+months = "Jan\nFeb\nMar\nApr\nMay\nJun\nJul\nAug"
+
+# Prints statements and calls the days and months variables to print on the same line by using the ,
+print "Here are the days: " , days
+print "Here are the months: " , months
+
+# Prints the paragraph written, but uses the triple quotes at the beginning and then again at the end, which prints everything in between.
+print """
+There's something going on here.
+With the three double quotes.
+We'll be able to type as much as we like.
+Even 4 lines if we want, or 5, or 6.
+"""
+
