b123c32c4ed6948837eeb120c14d49d6c6533918
Alexandra McCoy <mccoyale@gmail.com>
2019-04-03 21:26:40 -0400

    Control Structures

---
+++ golang/golang-book/chapter5/main.go
@@ -0,0 +1,28 @@
+package main
+
+import "fmt"
+
+/*Commenting this section out. This is the sample code that will turn into a control loop
+
+func main() {
+  fmt.Println(1)
+  fmt.Println(2)
+  fmt.Println(3)
+  fmt.Println(4)
+  fmt.Println(5)
+  fmt.Println(6)
+  fmt.Println(7)
+  fmt.Println(8)
+  fmt.Println(9)
+  fmt.Println(10)
+
+*/
+
+func main() {
+  i := 1
+  for i <= 10 { 
+    fmt.Println(i)
+	i = i + 1
+}
+
+}
---
+++ golang/golang-book/chapter5/main2.go
@@ -0,0 +1,19 @@
+package main 
+
+import "fmt"
+
+func main() {
+	for i := 1; i <= 100; i++ {
+		if i % 3 == 0 {
+			fmt.Println("Fizz")
+		} else if i % 5 == 0 {
+			fmt.Println("Buzz")
+		} else {
+			fmt.Println(i)
+		}
+	}
+}
+
+// This program prints Fizz for numbers divisible by 3 and Buzz for numbers divisible by 5, while printing the number for ones that are neither.
+
+// I cannot figure out how to print FizzBuzz for numbers divisible by both 3 and 5. But need to include that. 
